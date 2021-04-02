8a0a486373a0dd65a65a229f4a1a05f9e931f045
Sergio-Al <54541807+Sergio-Al@users.noreply.github.com>
2021-04-02 19:33:49 -0400

    importing npm modules
    In this lesson we did the following:
    - We installed via yarn (yarn add) the next modules react react-dom validator.
    - We saw the documentation (via npm and official) for the implementation via es6 syntax (import ...)
    - All code executed successfully.

--- src/app.js
+++ src/app.js
@@ -1,29 +1,6 @@
-// import './utils';
-// import subtract, { square, add } from "./utils.js";
+// install -> import -> use
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-// console.log("app.js is running!");
-// console.log(add(square(4), 3));
-// console.log(subtract(100, 80));
-
-import isSeniorCitizen, { isAdult, canDrink, iAmConstant } from "./person.js";
-
-const person = {
-  name: "little boy ",
-  age: 25,
-};
-console.log(
-  isAdult(person.age)
-    ? `you are adult ${person.name}`
-    : `you aren't adult ${person.name}`
-);
-console.log(
-  canDrink(person.age)
-    ? `You can drink ${person.name}`
-    : `you can't drink ${person.name}`
-);
-console.log(`this is a simple constant ${iAmConstant + 23}`);
-console.log(
-  isSeniorCitizen(person.age)
-    ? `You are a Senior ${person.name}`
-    : `You aren't a senior ${person.name}`
-);
+const template = React.createElement('button', {}, 'This is my code');
+ReactDOM.render(template, document.getElementById('app'));
