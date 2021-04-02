139ee64e6d7e2701fa1a5c97ed3818e6510f8077
Sergio-Al <54541807+Sergio-Al@users.noreply.github.com>
2021-04-02 18:11:58 -0400

    import/export es6
    This lesson contains the following advances:
    - We review the concepts of import/export of es6 in Javascript.
    - We created a new file utils.js to view the main features of export variables containing functions or data that we can interact in other js files just importing it.
    - we imported the new file utils.js with our variables in the respective format to app.js for use our new variables
    - we printed our new imported variables and then we do an exercise.
    - The exercise show us the level of manipulation of imported variables from other file.
    - The code executed successfully.

--- src/app.js
+++ src/app.js
@@ -1,1 +1,14 @@
-console.log('app.js is running!');
+// import './utils';
+// import { square, add } from "./utils.js";
+
+// console.log("app.js is running!");
+// console.log(add(square(4), 3));
+import { isAdult, canDrink, iAmConstant } from "./person.js";
+
+const person = {
+    name: 'big boy',
+    age: 25
+};
+console.log(isAdult(person.age) ? `you are adult ${person.name}` : `you aren't adult ${person.name}`);
+console.log(canDrink(person.age) ? `You can drink ${person.name}` : `you can't drink ${person.name}`);
+console.log(`this is a simple constant ${iAmConstant + 23}`);
---
+++ src/person.js
@@ -0,0 +1,7 @@
+console.log("person is running");
+
+const isAdult = (age) => age >= 18;
+const canDrink = (age) => age >= 21;
+const iAmConstant = 23;
+
+export { isAdult, canDrink, iAmConstant };
