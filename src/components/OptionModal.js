4af900619cc471e5c42220c3677fb23e8cc24114
Sergio-Al <54541807+Sergio-Al@users.noreply.github.com>
2021-04-05 22:16:13 -0400

    Styling complete
    - We stylized the modal component inspecting the element that the package is using.
    - We added animation and transition to the modal component.
    - We added reponsive patterns to our scss documents.
    - We added a custom icon to the app.
    - All code were executed successfully.

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -10,10 +10,16 @@ const OptionModal = (props) => (
     }
     onRequestClose={props.deleteSelected}
     contentLabel="Selected Option"
+    closeTimeoutMS={200}
+    className="modal"
   >
-    <h3>Selected Option</h3>
-    {props.selectedOption && <p>{props.selectedOption}</p>}
-    <button onClick={props.deleteSelected}>Okay</button>
+    <h3 className="modal__title">Selected Option</h3>
+    {props.selectedOption && (
+      <p className="modal__body">{props.selectedOption}</p>
+    )}
+    <button className="button" onClick={props.deleteSelected}>
+      Okay
+    </button>
   </Modal>
 );
 
